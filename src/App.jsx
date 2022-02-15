7693c1b0e56a5c940d36951877859e9affb47a75
manojxk <112118034@nitt.edu>
2022-02-15 17:06:24 +0530

    day_5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-      {/* <Navbar title="TextUtils" /> */}
       <div className="container my-5">
         <TextForm />
       </div>
