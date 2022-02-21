43f81b03df12bf84439e058ba2313745867da5dd
manojxk <112118034@nitt.edu>
2022-02-21 12:56:18 +0530

    added socket

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-// server.js
 const express = require("express");
-const { v4: uuidv4 } = require('uuid');
-console.log(uuidv4());
-
 const app = express();
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
 
-app.get('/', function (req, res) {
-    // NEW CODE
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
-})
+});
+
 app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room });
 });
 
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
 
-
-server.listen(3030);
+server.listen(process.env.PORT || 3030);
