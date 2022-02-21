a10aa0450eea9a18a555cb57d2d506d5575ccb54
manojxk <112118034@nitt.edu>
2022-02-21 10:46:18 +0530

    added routes and video stream

---
+++ public/script.js
@@ -0,0 +1,20 @@
+let myVideoStream;
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+})
+    .then((stream) => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream);
+    });
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+        videoGrid.append(video);
+    });
+};
